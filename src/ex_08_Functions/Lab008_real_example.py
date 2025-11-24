df1072dc10a9a5d30b2574c6cfc896697b8901b2
ankesh <ashikhar6@gmail.com>
2025-11-25 02:59:14 +0530

    Assignment_30th_october

--- src/ex_08_Functions/Lab008_real_example.py
+++ src/ex_08_Functions/Lab008_real_example.py
@@ -1,11 +1,12 @@
 def validate_status_code(response_code):
+    """Validate if the response code is 200."""
     if response_code > 0:
-     """Validate if the response code is 200."""
         if response_code == 200:
             print("Valid response code:", response_code)
         else:
             print("Invalid response code:", response_code)
     else:
         print("Please enter a positive response code.")
-validate_status_code(200)  # True
-validate_status_code(404)  # False
+
+validate_status_code(200)  # Valid
+validate_status_code(404)  # Invalid
