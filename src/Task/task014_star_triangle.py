267d353e81bf9f7e959e464d50934037fd305ad9
ankesh <ashikhar6@gmail.com>
2026-01-20 03:12:01 +0530

    Updated way

--- src/Task/task014_star_triangle.py
+++ src/Task/task014_star_triangle.py
@@ -3,8 +3,16 @@
 #* * *
 #* * * *
 #* * * * *
-def print_star_triangle(n):
-    for i in range(1, n + 1):
-        print('* ' * i)
-# Example usage
-print_star_triangle(5)
+# def print_star_triangle(n):
+#     for i in range(1, n + 1):
+#         print('* ' * i)
+# # Example usage
+# print_star_triangle(5)
+
+
+row = int(input("enter the value for the right angle triangle"))
+
+for i in range(1, row + 1):
+    for j in range(i):
+        print('* ', end="")
+    print()
