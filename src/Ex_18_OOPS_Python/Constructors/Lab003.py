233fc109d9918629dce5fc8acda785b209add4f2
ankesh <ashikhar6@gmail.com>
2026-01-19 23:59:23 +0530

    Constructors

---
+++ src/Ex_18_OOPS_Python/Constructors/Lab003.py
@@ -0,0 +1,27 @@
+class Dog:
+#attribute
+    name = None
+    breed = None
+    height = None
+    weight = None
+#behavior
+    #passing parameters
+
+    def __init__(self):
+    # default constructor
+        print("this will be called")
+
+
+    def bark(self):
+
+        print("barking")
+    def sleep(self):
+        print("sleep  ")
+    def talk(self):
+        pass
+
+chow_ref = Dog()
+rancho_ref = Dog()
+
+print(chow_ref.name)
+print(rancho_ref.breed)
