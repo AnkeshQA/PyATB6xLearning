2baf916b597b5896803ab0d0b6d74cdaf0f67479
ankesh <ashikhar6@gmail.com>
2026-01-21 03:40:19 +0530

    Pytest

--- src/Ex_25Allure_Pytest/test_lab001.py
+++ src/Ex_25Allure_Pytest/test_lab001.py
@@ -1,2 +1,21 @@
 #install allure-pytest
 # pip install allure-pytest
+
+import allure
+import pytest
+
+
+@allure.title("Verify create booking - negative scenario")
+@allure.description("Verify create booking fails with invalid data")
+@pytest.mark.negative
+def test_method1():
+    print("hello world")
+    assert 4 - 1 == 6   # expected failure
+
+
+@allure.title("Verify login - positive scenario")
+@allure.description("Verify login works with valid credentials")
+@pytest.mark.positive
+def test_login():
+    print("hello world")
+    assert 1 + 1 == 2
